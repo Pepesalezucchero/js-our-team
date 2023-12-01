327898b75bc5af7c7a0a82d0500985e0c87219bf
Alessio Rugi <alessiorugi95@gmail.com>
2023-12-01 15:59:44 +0100

    array di oggetti creato

--- js/script.js
+++ js/script.js
@@ -5,3 +5,37 @@ Creare l’array di oggetti con le informazioni fornite.
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 Stampare le stesse informazioni su DOM sotto forma di stringhe
 */
+
+//creare un array di oggetti per rappresentare i membri del team.
+const arrayTeam = [
+    {
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO",
+        "foto": "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor",
+        "foto": "angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "foto": "walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "foto": "angela-gordon-social-media-manager.jpg"
+    },
+    {
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "foto": "scott-estrada-developer.jpg"
+    },
+    {
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "foto": "barbara-ramos-graphic-designer.jpg"
+    },
+]
